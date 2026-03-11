e3721bd26ac105c060341b8f42a4daa41dac1c87
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2026-03-11 21:58:23 +0100

    Updated live status and included local time

--- src/components/BioCard.jsx
+++ src/components/BioCard.jsx
@@ -3,7 +3,7 @@ export default function BioCard() {
     <p>
       Hi, I’m <span className="bold-style">Shantel</span> — a
       <span className="bold-style"> Frontend Developer</span> who loves turning
-      <span className="italic-style">technical logic</span> into vibrant,
+      <span className="italic-style"> technical logic</span> into vibrant,
       storytelling driven interfaces. I enjoy building
       <span className="bold-style"> ethical digital spaces</span> that connect
       people through <span className="bold-style">high-end design.</span>
--- src/components/StatusCard.jsx
+++ src/components/StatusCard.jsx
@@ -1,22 +1,47 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+const formatter = new Intl.DateTimeFormat("en-GB", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: true,
+  timeZone: "Europe/Berlin",
+});
 
 export default function StatusCard() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
   useEffect(() => {
-    fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=53.5507&longitude=9.993&current=temperature_2m,weather_code`,
-    )
-      .then((res) => res.json())
-      .then((data) => console.log(data.current));
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
   }, []);
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.open-meteo.com/v1/forecast?latitude=53.5507&longitude=9.993&current=temperature_2m,weather_code`,
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data.current));
+  // }, []);
 
   return (
     <>
       <div className="status">
-        <div className="status-dot"></div>
+        <div className="status-dot">
+          <div className="status-dot-active"></div>
+        </div>
         <p>currently coding.</p>
       </div>
-      <div className="lok-time"></div>
-      <div className="lok-weather"></div>
+
+      <div className="local-weather"></div>
+      <div className="local-time">
+        <span className="current-time">
+          {formatter.format(currentTime)} HAM{" "}
+        </span>
+      </div>
     </>
   );
 }
