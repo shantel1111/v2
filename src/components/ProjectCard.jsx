ad0c8441fdc130437159ac5a81dedc34e2d30437
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2026-03-14 23:53:07 +0100

    Project UI framework

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,25 @@
+import quiz from "../assets/quizzical.png";
+import movie from "../assets/movie-watchlist.png";
+import pswrd from "../assets/password-generator.png";
+
 export default function ProjectCard() {
   return (
     <>
       <h3>projects.</h3>
+      <div className="projects-container">
+        <div className="project-names">
+          <h4>quizzical</h4>
+          <h4>movie</h4>
+          <h4>pswrd</h4>
+        </div>
+
+        <div className="project-imgs"></div>
+
+        <div className="project-icons">
+          <i class="fa-solid fa-code"></i>
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </div>
+      </div>
     </>
   );
 }
