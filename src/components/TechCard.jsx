9b7871ad532c6a13e23884b8268e64900d3a956b
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2026-03-19 23:13:58 +0100

    New React icons

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -1,3 +1,22 @@
+import { DiReact, DiHtml5, DiCss3, DiJavascript1, DiGit } from "react-icons/di";
+import { SiTailwindcss, SiFigma, SiTypescript } from "react-icons/si";
+
 export default function TechCard() {
-  return <></>;
+  return (
+    <div>
+      <div className="tech-stack1">
+        <DiHtml5 className="html-icon icon-size " />
+        <DiCss3 className="css-icon icon-size" />
+        <DiJavascript1 className="js-icon icon-size" />
+        <SiTypescript className="typescript-icon icon-size" />
+      </div>
+
+      <div div className="tech-stack1">
+        <DiReact className="react-icon icon-size" />
+        <SiTailwindcss className="tailwind-icon icon-size" />
+        <DiGit className="git-icon icon-size" />
+        <SiFigma className="figma-icon icon-size" />
+      </div>
+    </div>
+  );
 }
